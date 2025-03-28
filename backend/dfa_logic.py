559c28ab6e540fae9bcdac50572da4cada145fa3
Neil Artus PC <neil.c.artus@gmail.com>
2025-03-28 15:11:16 +0800

    add stars dfa

--- backend/dfa_logic.py
+++ backend/dfa_logic.py
@@ -44,32 +44,34 @@ class DFA:
         # unsure if this covers all cases and edge cases, need more testing 
 
         for symbol in input_string:
-            if symbol not in self.alphabet:
-                print(f'Simulation Error: Symbol "{symbol}" not in alphabet {self.alphabet}.')
-                state_sequence.append('REJECT_STATE_INVALID_SYMBOL')
+            try: 
+                if symbol not in self.alphabet:
+                    state_sequence.append('REJECT_STATE_INVALID_SYMBOL')
+                    raise ValueError(f'Simulation Error: Symbol "{symbol}" not in alphabet {self.alphabet}.')
 
-            state_transitions = self.transitions.get(current_state)
+                state_transitions = self.transitions.get(current_state)
 
-            if state_transitions is None:
-                print(f'Simulation Error: State "{current_state}" has no defined transitions.')
-                state_sequence.append('REJECT_STATE_NO_TRANSITION')
+                if state_transitions is None:
+                    state_sequence.append('REJECT_STATE_NO_TRANSITION')
+                    raise ValueError(f'Simulation Error: State "{current_state}" has no defined transitions.')
 
-            next_state = state_transitions.get(symbol)
+                next_state = state_transitions.get(symbol)
 
-            if next_state is None:
-                print(f'Simulation Error: No transition defined for state "{current_state}" on symbol "{symbol}".')
-                state_sequence.append('REJECT_STATE_MISSING_TRANSITION')
+                if next_state is None:
+                    state_sequence.append('REJECT_STATE_MISSING_TRANSITION')
+                    raise ValueError(f'Simulation Error: "{next_state}" has no defined state transition. Set a valid state transition for all cases.')
+                if next_state in self.trap_states:
+                    state_sequence.append('REJECT_STATE_TRAP_STATE')
+                    raise ValueError(f'Simulation Error: Transition leads to trap state {next_state}.')
 
-            if next_state in self.trap_states:
-                print(f'Simulation Error: Transition leads to trap state {next_state}.')
-                state_sequence.append('REJECT_STATE_TRAP_STATE')
-
-            if next_state not in self.states:
-                 print(f'Internal DFA Error: Transition leads to undefined state "{next_state}".')
-                 state_sequence.append('REJECT_STATE_INVALID_TARGET')
-
-            current_state = next_state
-            state_sequence.append(current_state)
+                if next_state not in self.states:
+                    state_sequence.append('REJECT_STATE_INVALID_TARGET')
+                    raise ValueError(f'Internal DFA Error: Transition leads to undefined state "{next_state}".')
+    
+                current_state = next_state
+                state_sequence.append(current_state)
+            except ValueError as ve:
+                return (f'{ve}')
 
         is_accepted = current_state in self.final_states
 
@@ -90,14 +92,18 @@ class DFA:
             'alphabet': sorted(list(self.alphabet)),
             'transitions': self.transitions,
             'start_state': self.start_state,
-            'final_states': sorted(list(self.final_states))
+            'final_states': sorted(list(self.final_states)),
+            'trap_states': sorted(list(self.trap_states))
         }
 
 
 if __name__ == '__main__':
 
-    # references/bets_dfa.png
-    bets_dfa = {
+    # temporary spaghetti code, this will do for now. will reorganize once packaged as a Flask app.
+
+    bets_states = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+    bets_alphabet = {'a', 'b'}
+    bets_transitions = {
         0: {'a': 2, 'b': 1},
         1: {'a': 3, 'b': 3},               
         2: {'a': 3, 'b': 4},               
@@ -112,29 +118,87 @@ if __name__ == '__main__':
         11: {'a': 12, 'b': 12},  
         12: {'a': 12, 'b': 12}
     }
-
-    bets_states = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
-    trap_states = {7, 8}
+    bets_start = 0
+    bets_final = {12}
+    bets_trap = {7, 8}
 
     try:
-        my_dfa = DFA(
+        bets_dfa = DFA(
             states=bets_states,
-            alphabet={'a', 'b'},
-            transitions=bets_dfa,
-            start_state=0,
-            final_states={12},
-            trap_states=trap_states
-        )
-        print('DFA Compiled Successfully.')
+            alphabet=bets_alphabet,
+            transitions=bets_transitions,
+            start_state=bets_start,
+            final_states=bets_final,
+            trap_states=bets_trap
+            )
+        print(f'Bets DFA Compiled Successfully.')
     except Exception as e:
-        print(f'Error Compiling the DFA. Please adhere to the formatting specified by the docstrings: {e}')
-    
-    print('--- www.bestdfacompiler.netlify.app ---') 
+        print(f'Error Compiling Bets DFA. Please adhere to the formatting specified by the docstrings: {e}')
+
+    # pls double check ty hehe
+
+    stars_states = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24}
+    stars_alphabet = {'0','1'}
+    stars_transitions = {
+        0:{'0': 1, '1':2},
+        1:{'0': 5, '1':3},
+        2:{'0': 5, '1':5},
+        3:{'0': 6, '1':4},
+        4:{'0': 4, '1':4},
+        5:{'0': 4, '1':6},
+        6:{'0': 7, '1':7},
+        7:{'0': 9, '1':8},
+        8:{'0': 9, '1':12},
+        9:{'0': 12, '1':8},
+        10:{'0': 10, '1':11},
+        11:{'0': 22, '1':12},
+        12:{'0': 9, '1':17},
+        13:{'0': 15, '1':8},
+        14:{'0': 10, '1':11},
+        15:{'0': 14, '1':16},
+        16:{'0': 22, '1':12},
+        17:{'0': 20, '1':19},
+        18:{'0': 15, '1':8},
+        19:{'0': 23, '1':19},
+        20:{'0': 18, '1':21},
+        21:{'0': 9, '1':12},
+        22:{'0': 13, '1':8},
+        23:{'0': 18, '1':21},
+        }
+    stars_start = 0
+    stars_final = {10, 11, 18, 21, 22, 23}
+    stars_trap = {4}
+
+    try :
+        stars_dfa = DFA(
+            states = stars_states,
+            alphabet = stars_alphabet,
+            transitions= stars_transitions,
+            start_state= stars_start,
+            final_states= stars_final,
+            trap_states= stars_trap 
+        )
+        print('Stars DFA compiled successfully.')
+    except Exception as e:
+        print(f'Error Compiling Bets DFA. Please adhere to the formatting specified by the docstrings: {e}')
+
+
+    print('--- DFA Compiler CLI Mockup ---') 
     while True:
+        print('1. (111 + 101 + 001 + 010) (1 + 0 + 11)(1 + 0 + 11)* (111 + 000) (111 + 000)* (01 + 10 + 00)')
+        print('2. (aa + bb + aba + ba) (aba + bab + bbb) (a + b)* (a + b + aa + abab) (aa + bb)*')
+        choice = int(input('Choose a DFA to simulate.'))
         word = str(input('Enter a word to validate : '))
+        if choice == 1:
+            accepted = stars_dfa.simulate(word)
+        elif choice ==2: 
+            accepted = bets_dfa.simulate(word)
+        else:
+            print('Please choose a valid DFA to simulate.')
+            continue
         if word.lower() == 'exit':
             exit()
-        accepted = my_dfa.simulate(word)
+        accepted = bets_dfa.simulate(word)
     
 
 
